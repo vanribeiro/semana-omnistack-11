99a5f80155bab0e6b68986785a5a4cebd7072526
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-03-27 00:47:18 -0300

    correct incidentsection. add delete function.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -23,11 +23,23 @@ export default function Profile() {
 
     }, [ongId]);
 
+    async function handleDeleteIncident(id) {  
+        try {
+            await api.delete(`incidents/${id}`,{
+                headers: {
+                    Authorization: ongId
+                }
+            });
+        } catch (error) {
+            alert('Erro ao deletar o caso... Tente novamente!');
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logo} alt="Be The Hero"/>
-                <span>Bem-vindo, {ongName}</span>
+                <span>Bem-vindo, {ongName}!</span>
                 <Link className="button" to="/incidents/new">
                     Cadastrar Novo Caso
                 </Link>
@@ -49,9 +61,9 @@ export default function Profile() {
                             <p>{incident.description}</p>
                             
                             <strong>Valor:</strong>
-                            <p>{incident.value}</p>
+                            <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-                            <button type="button">
+                            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                                 <FiTrash2 size={20} color="#a8a8b3" />
                             </button>
                         </li>
